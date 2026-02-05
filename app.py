13d9d55f041737fe6f908c5b3c7053ace940d0bb
stephentwig <step.twig@gmail.com>
2026-02-04 21:09:00 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import os
+import random
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    welcome_text ="This is my application"  # Simple computation
+    return welcome_text
+
+
+def multiply():
+    a = 3
+    b = 4
+    return a * b
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
